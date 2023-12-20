9bdc9c7d7a024334abc5d38471eda29661c23617
Salvatore <mammoliti.sam@gmail.com>
2023-12-20 17:12:03 -0500

    Equality

---
+++ Lesson 6 - Equality/main.js
@@ -0,0 +1,10 @@
+// Equality
+
+// The strict equality (===) operator checks whether its two operands are equal, returning a Boolean result. 
+// Unlike the equality operator, the strict equality operator always considers operands of different types 
+// to be different.
+
+console.log(1 === 1);
+console.log("hello" === "hello");
+console.log("1" === 1);
+console.log(0 === false);
